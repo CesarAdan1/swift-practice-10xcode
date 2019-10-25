2fe70bcbfc30a5319cc3c9a9095a72a9dbebf048
Cesar_Adan <whoclash@hotmail.com>
2019-10-25 16:44:05 -0500

    otherappxcode10

---
+++ woperti-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  woperti-ios
+//
+//  Created by Cesar Juarez on 8/16/19.
+//  Copyright © 2019 Cesar Juarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
